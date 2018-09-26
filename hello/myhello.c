a437f4b37d73fda1c0f45731596de3ba340455ef
Kawaharasouta <kawahara6514@gmail.com>
2018-09-26 14:30:02 +0900

    lesson1

---
+++ hello/myhello.c
@@ -0,0 +1,18 @@
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Kawaharasouta <kawahara6514@gmail.com>");
+MODULE_DESCRIPTION("my first module. Hello world kernel module.");
+
+static int mymodule_init(void) {
+	printk(KERN_ALERT "Hello world!!!\n");
+	return 0;
+}
+
+static void mymodule_exit(void) {
+	printk(KERN_ALERT "bye\n");
+	return 0;
+}
+
+module_init(mymodule_init);
+module_exit(mymodule_exit);
---
+++ timer/mytimer1.c
@@ -0,0 +1,34 @@
+#include <linux/module.h>
+#include <linux/timer.h>
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Kawaharasouta <kawahara6514@gmail.com>");
+MODULE_DESCRIPTION("timer kernel module: oneshot timer");
+
+struct timer_list mytimer;
+
+#define MYTIMER_TIMEOUT_SECS	10
+
+static void mytimer_fn(unsigned long arg)
+{ 
+	printk(KERN_ALERT "10 secs passed.\n");
+}
+
+static int mymodule_init(void)
+{
+	init_timer(&mytimer);
+	mytimer.expires = jiffies + MYTIMER_TIMEOUT_SECS*HZ;
+	mytimer.data = 0;
+	mytimer.function = mytimer_fn;
+	add_timer(&mytimer);
+
+	return 0;
+}
+
+static void mymodule_exit(void)
+{ 
+	del_timer(&mytimer);
+}
+
+module_init(mymodule_init);
+module_exit(mymodule_exit);
