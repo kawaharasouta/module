1d1e3e079a35721b575c5b23d7cde2587b9467cb
Kawaharasouta <kawahara6514@gmail.com>
2019-03-01 14:13:38 +0900

    save

--- pci_exp/exp/pci.c
+++ pci_exp/exp/pci.c
@@ -3,8 +3,8 @@
 #include<linux/kernel.h>
 #include<linux/pci.h>
 
-#include<errno.h>
-#include<ioport.h>
+#include<linux/errno.h>
+#include<linux/ioport.h>
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Kawaharasouta <kawahara6514@gmail.com>");
@@ -28,7 +28,7 @@ print_pci_info(void) {
 	struct pci_dev *device = NULL;
 
 	while (1) {
-		device = pci_find_device(PCI_ANY_ID, PCI_ANY_ID, device);
+		device = pci_get_device(PCI_ANY_ID, PCI_ANY_ID, device);
 
 		if (device == NULL) 
 			break;
@@ -47,12 +47,12 @@ print_pci_info(void) {
 		pci_read_config_word(device, PCI_DEVICE_ID, &device_id);
 		pci_read_config_word(device, PCI_CLASS_DEVICE, &class);
 		pci_read_config_word(device, PCI_SUBSYSTEM_VENDOR_ID, &sub_vendor_id);
-		pci_read_config_word(device, PCI_SUBSYSTEM_ID, &sub_deivce_id);
-		pci_read_config_word(device, PCI_INTERRUPT_LINE, &irq);
+		pci_read_config_word(device, PCI_SUBSYSTEM_ID, &sub_device_id);
+		pci_read_config_byte(device, PCI_INTERRUPT_LINE, &irq);
 
 		printk(KERN_INFO "%s: vendor id = %i, device_id = %i\n", msg, vendor_id, device_id);
 		printk(KERN_INFO "%s: class = %i\n", msg, class);
-		printk(KERN_INFO "%s: ***subsystem***\n");
+		printk(KERN_INFO "%s: ***subsystem***\n", msg);
 		printk(KERN_INFO "%s: vendor id = %i, device_id = %i\n", msg, sub_vendor_id, sub_device_id);
 		printk(KERN_INFO "%s: IRQ = %i\n", msg, irq);
 
@@ -60,7 +60,7 @@ print_pci_info(void) {
 		for (i = 0; i < BASE_ADDRESS_NUM; i++) {
 			resource_start = pci_resource_start(device, i);
 			resource_end = pci_resource_end(device, i);
-			resource_flag = pci_resource_flag(device, i);
+			resource_flag = pci_resource_flags(device, i);
 
 			if (resource_start != 0 || resource_end != 0) {
 				printk(KERN_INFO "%s: Base address 0x%0x - 0x%0x\n", msg, resource_start, resource_end);
@@ -77,11 +77,11 @@ print_pci_info(void) {
 				if (resource_flag & IORESOURCE_READONLY)
 					printk(KERN_INFO "%s: Read Only\n", msg);
 
-				printk(KERN_INFO "%s: \n",msg);
+				printk(KERN_INFO "%s: \n", msg);
 			}
 		}
 
-		printk(KERN_INFO "%s: \n",msg);
+		printk(KERN_INFO "%s: \n", msg);
 	}
 }
 
