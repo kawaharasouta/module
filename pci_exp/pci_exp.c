8fc1f3922cb765fd68f5f0d6d9ac3b8eb6627520
Kawaharasouta <kawahara6514@gmail.com>
2019-03-07 15:12:18 +0900

    save

--- pci_exp/pci_exp.c
+++ pci_exp/pci_exp.c
@@ -5,25 +5,60 @@
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Kawaharasouta <kawahara6514@gmail.com>");
-MODULE_DESCRIPTION("my first module. Hello world kernel module.");
+MODULE_DESCRIPTION("network module");
 
-char pci_exp_name[] = "pci_exp";
+static char *pci_exp_msg = "module [pci_exp]:";
+char *pci_exp_name = "pci_exp";
+
+
+
+static int 
+pci_exp_init_one(struct pci_dev *pdev, const struct pci_device_id *ent) {
+
+
+}
+
 
 
 #if 1
 //struct pci_device_id pci_device_id;
 struct pci_driver pci_exp_driver = {
 	.name = pci_exp_name,
-	.id_table = 
-	.probe = 
-	.remove = 
+	.id_table =  // The device list this driver support.
+	.probe = pci_exp_init_one // It is called at device detection if it is a prescribed driver, or at (possibly) modprobe otherwise.
+	.remove =  // It is called at device unload.
+#ifdef CONFIG_PM //When the power control is ON.
+	.suspend =
+	.resume = 
+#endif /* CONFIG_PM */
 }
 #endif
 
 
+
+static const struct net_device_ops pci_exp_ops = {
+	.ndo_open =
+	.ndo_stop = 
+	.ndo_start_xmit = 
+	.ndo_get_stats =
+	.ndo_set_rx_mode = 
+//	.ndo_set_mac_address = 
+	.ndo_tx_timeout = 
+//	.ndo_change_mtu = 
+	.ndo_do_ioctl = 
+	.ndo_validate_addr = 
+//	.ndo_vlan_rx_add_vid =
+//	.ndo_vlan_rx_kill_vid =
+	.ndo_poll_controller =
+	.ndo_fix_features =
+	.ndo_set_features =
+}
+
+
+
 static int 
 pci_exp_init(void) {
-	printk(KERN_ALERT "loading pci_exp.\n");
+	printk(KERN_ALERT "%s loading pci_exp.\n", pci_exp_msg);
 	return 0;
 
 	int ret;
@@ -33,7 +68,7 @@ pci_exp_init(void) {
 }
 static void 
 pci_exp_exit(void) {
-	printk(KERN_ALERT "pci_exp bye.\n");
+	printk(KERN_ALERT "%s pci_exp bye.\n", pci_exp_msg);
 
 	int ret;
 	ret = pci_unregister_driver(&pci_exp_driver);
